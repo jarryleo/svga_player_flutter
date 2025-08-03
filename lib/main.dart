fe5f7c09985bbbed5f10188311f2083efaf365ab
JarryLeo <yjtx256@gmail.com>
2025-08-03 18:49:56 +0800

    [add] WEB支持

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:svga_player_flutter/svga_viewer.dart';
-import 'package:svga_player_flutter/svgaplayer/svga_source.dart';
-import 'package:svga_player_flutter/svgaplayer/svgaplayer_flutter.dart';
+import 'package:svga_player_flutter/utils/platform_utils.dart';
 import 'package:window_manager/window_manager.dart';
+
+import 'page/home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // Must add this line.
-  await windowManager.ensureInitialized();
+  if (PlatFormUtils.isDesktop()) {
+    // Must add this line.
+    await windowManager.ensureInitialized();
 
-  WindowOptions windowOptions = const WindowOptions(
-    size: Size(1000, 700),
-    center: true,
-    skipTaskbar: false,
-    titleBarStyle: TitleBarStyle.normal,
-  );
-  windowManager.waitUntilReadyToShow(windowOptions, () async {
-    await windowManager.show();
-    await windowManager.focus();
-  });
+    WindowOptions windowOptions = const WindowOptions(
+      size: Size(1000, 700),
+      center: true,
+      skipTaskbar: false,
+      titleBarStyle: TitleBarStyle.normal,
+    );
+    windowManager.waitUntilReadyToShow(windowOptions, () async {
+      await windowManager.show();
+      await windowManager.focus();
+    });
+  }
 
   runApp(const ExampleApp());
 }
@@ -29,81 +31,5 @@ class ExampleApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(theme: ThemeData.dark(), home: HomeScreen());
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  final samples = const <String>[
-    "assets/angel.svga",
-    "assets/pin_jump.svga",
-    "https://cdn.jsdelivr.net/gh/svga/SVGA-Samples@master/EmptyState.svga",
-    "https://cdn.jsdelivr.net/gh/svga/SVGA-Samples@master/HamburgerArrow.svga",
-    "https://cdn.jsdelivr.net/gh/svga/SVGA-Samples@master/PinJump.svga",
-    "https://cdn.jsdelivr.net/gh/svga/SVGA-Samples@master/TwitterHeart.svga",
-    "https://cdn.jsdelivr.net/gh/svga/SVGA-Samples@master/Walkthrough.svga",
-    "https://cdn.jsdelivr.net/gh/svga/SVGA-Samples@master/kingset.svga",
-    "https://cdn.jsdelivr.net/gh/svga/SVGA-Samples@master/halloween.svga",
-    "https://cdn.jsdelivr.net/gh/svga/SVGA-Samples@master/heartbeat.svga",
-    "https://cdn.jsdelivr.net/gh/svga/SVGA-Samples@master/matteBitmap.svga",
-    "https://cdn.jsdelivr.net/gh/svga/SVGA-Samples@master/matteBitmap_1.x.svga",
-    "https://cdn.jsdelivr.net/gh/svga/SVGA-Samples@master/matteRect.svga",
-    "https://cdn.jsdelivr.net/gh/svga/SVGA-Samples@master/mutiMatte.svga",
-    "https://cdn.jsdelivr.net/gh/svga/SVGA-Samples@master/posche.svga",
-    "https://cdn.jsdelivr.net/gh/svga/SVGA-Samples@master/rose.svga",
-  ].map((e) => [e.split('/').last, e]).toList(growable: false);
-
-  // callback for register dynamicItem
-  final dynamicSamples = <String, void Function(MovieEntity entity)>{
-    "kingset.svga": (entity) => entity.dynamicItem
-      ..setText(
-          TextPainter(
-              text: const TextSpan(
-                  text: "Hello, World!",
-                  style: TextStyle(
-                    fontSize: 28,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ))),
-          "banner")
-    // ..setImageWithUrl(
-    //     "https://github.com/PonyCui/resources/blob/master/svga_replace_avatar.png?raw=true",
-    //     "99")
-    // ..setDynamicDrawer((canvas, frameIndex) {
-    //   canvas.drawRect(Rect.fromLTWH(0, 0, 88, 88),
-    //       Paint()..color = Colors.red); // draw by yourself.
-    // }, "banner"),
-  };
-
-  HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('SVGA Flutter Samples')),
-      body: ListView.separated(
-          itemCount: samples.length,
-          separatorBuilder: (_, __) => const Divider(),
-          itemBuilder: (context, index) {
-            return ListTile(
-                title: Text(samples[index].first),
-                subtitle: Text(samples[index].last),
-                onTap: () => _goToSample(context, samples[index]));
-          }),
-    );
-  }
-
-  void _goToSample(context, List<String> sample) {
-    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-      String name = sample.first;
-      String value = sample.last;
-      SVGASource source;
-      if (value.startsWith(RegExp(r'https?://'))) {
-        source = SVGASource.network(name, value);
-      } else {
-        source = SVGASource.asset(name, value);
-      }
-      return SVGASampleScreen(
-          source: source, dynamicCallback: dynamicSamples[sample.first]);
-    }));
   }
 }
---
+++ lib/page/home.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:svga_player_flutter/svgaplayer/svga_source.dart';
+import 'package:svga_player_flutter/svgaplayer/svgaplayer_flutter.dart';
+
+import 'svga_viewer.dart';
+
+class HomeScreen extends StatelessWidget {
+  final samples = const <String>[
+    "assets/angel.svga",
+    "assets/pin_jump.svga",
+    "https://cdn.jsdelivr.net/gh/svga/SVGA-Samples@master/EmptyState.svga",
+    "https://cdn.jsdelivr.net/gh/svga/SVGA-Samples@master/HamburgerArrow.svga",
+    "https://cdn.jsdelivr.net/gh/svga/SVGA-Samples@master/PinJump.svga",
+    "https://cdn.jsdelivr.net/gh/svga/SVGA-Samples@master/TwitterHeart.svga",
+    "https://cdn.jsdelivr.net/gh/svga/SVGA-Samples@master/Walkthrough.svga",
+    "https://cdn.jsdelivr.net/gh/svga/SVGA-Samples@master/kingset.svga",
+    "https://cdn.jsdelivr.net/gh/svga/SVGA-Samples@master/halloween.svga",
+    "https://cdn.jsdelivr.net/gh/svga/SVGA-Samples@master/heartbeat.svga",
+    "https://cdn.jsdelivr.net/gh/svga/SVGA-Samples@master/matteBitmap.svga",
+    "https://cdn.jsdelivr.net/gh/svga/SVGA-Samples@master/matteBitmap_1.x.svga",
+    "https://cdn.jsdelivr.net/gh/svga/SVGA-Samples@master/matteRect.svga",
+    "https://cdn.jsdelivr.net/gh/svga/SVGA-Samples@master/mutiMatte.svga",
+    "https://cdn.jsdelivr.net/gh/svga/SVGA-Samples@master/posche.svga",
+    "https://cdn.jsdelivr.net/gh/svga/SVGA-Samples@master/rose.svga",
+  ].map((e) => [e.split('/').last, e]).toList(growable: false);
+
+  // callback for register dynamicItem
+  final dynamicSamples = <String, void Function(MovieEntity entity)>{
+    "kingset.svga": (entity) => entity.dynamicItem
+      ..setText(
+          TextPainter(
+              text: const TextSpan(
+                  text: "Hello, World!",
+                  style: TextStyle(
+                    fontSize: 28,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ))),
+          "banner")
+    // ..setImageWithUrl(
+    //     "https://github.com/PonyCui/resources/blob/master/svga_replace_avatar.png?raw=true",
+    //     "99")
+    // ..setDynamicDrawer((canvas, frameIndex) {
+    //   canvas.drawRect(Rect.fromLTWH(0, 0, 88, 88),
+    //       Paint()..color = Colors.red); // draw by yourself.
+    // }, "banner"),
+  };
+
+  HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('SVGA Flutter Samples')),
+      body: ListView.separated(
+          itemCount: samples.length,
+          separatorBuilder: (_, __) => const Divider(),
+          itemBuilder: (context, index) {
+            return ListTile(
+                title: Text(samples[index].first),
+                subtitle: Text(samples[index].last),
+                onTap: () => _goToSample(context, samples[index]));
+          }),
+    );
+  }
+
+  void _goToSample(context, List<String> sample) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+      String name = sample.first;
+      String value = sample.last;
+      SVGASource source;
+      if (value.startsWith(RegExp(r'https?://'))) {
+        source = SVGASource.network(name, value);
+      } else {
+        source = SVGASource.asset(name, value);
+      }
+      return SVGASampleScreen(
+          source: source, dynamicCallback: dynamicSamples[sample.first]);
+    }));
+  }
+}
